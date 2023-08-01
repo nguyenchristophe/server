36e8967f1ad56698cfe1bf2407d77064b09da022
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-08-01 15:37:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 
 
 
-var server = app.listen(process.env.PORT || 80,function(){
+var server = app.listen(process.env.PORT || 3000,function(){
 
 var port = server.address().port;
 console.log("Expfress is working onport") + port;
